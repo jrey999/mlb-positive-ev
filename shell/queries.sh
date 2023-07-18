0b6b5e956d7ac7fb4030bf5a5b43bcf4e05172fb
jgr1204 <reynoldsjohngreg@gmail.com>
2023-07-18 16:52:31 -0400

    use toRST :)

--- shell/queries.sh
+++ shell/queries.sh
@@ -1,7 +1,7 @@
 script_dir="$(cd "$(dirname "$0")" && pwd)"; folder="$script_dir/../data/queries"
 for file in "$folder"/*; do
   
-  sqlite3 -header -csv db.sqlite3 < "$file" > temp.csv
-  csv2md temp.csv > $folder/../reports/$(basename "$file" .sql).md
-  rm temp.csv
+  sqlite3 -header -csv db.sqlite3 < "$file" > $(basename "$file" .sql).csv
+  torst $(basename "$file" .sql).csv -o $folder/../reports/
+  rm $(basename "$file" .sql).csv
 done
