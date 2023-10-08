6f46f92a228bd169ad3ff7680d38d47a95df0f4a
jgr1204 <reynoldsjohngreg@gmail.com>
2023-10-07 23:43:50 -0400

    temp

--- dags/etl.py
+++ dags/etl.py
@@ -4,21 +4,22 @@ from airflow.operators.python import PythonOperator
 from odds.datamodels import write_objects, dump_objects
 
 
-with DAG(
+dag = DAG(
     "my-dag",
     start_date=datetime(2023, 10, 6),
     schedule_interval="@daily",
     catchup=False
-) as dag:
+)
 
-    dump_data = PythonOperator(
-        task_id="dump-data",
-        python_callable=write_objects
-    )
+dump_data = PythonOperator(
+    task_id="dump-data",
+    python_callable=write_objects,
+    dag=dag
+)
 
-    write_data = PythonOperator(
-        task_id="write-data",
-        python_callable=dump_objects
-    )
+write_data = PythonOperator(
+    task_id="write-data",
+    python_callable=dump_objects
+)
 
 dump_data >> write_data
